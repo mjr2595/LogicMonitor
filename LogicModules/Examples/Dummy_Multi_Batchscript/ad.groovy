634118714c265b278532f43c19411d9369993b46
mjr2595 <michaeljohnraymond@gmail.com>
2024-02-15 17:48:25 -0600

    Added: Dummy example DS and multi-instance batchscript DS

---
+++ LogicModules/Examples/Dummy_Multi_Batchscript/ad.groovy
@@ -0,0 +1,4 @@
+println "instance1_value##instance1_alias##instance1_description"
+println "instance2_value##instance2_alias##instance2_description"
+println "instance3_value##instance3_alias##instance3_description"
+println "instance4_value##instance4_alias##instance4_description"
