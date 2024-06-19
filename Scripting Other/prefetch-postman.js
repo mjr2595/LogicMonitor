6a85cb08b102d748caf56c4d27538140bec42dce
mjr2595 <michaeljohnraymond@gmail.com>
2024-06-19 12:23:44 -0500

    Add super verbose comments to prefetch script

--- Scripting Other/prefetch-postman.js
+++ Scripting Other/prefetch-postman.js
@@ -10,21 +10,30 @@ Note: this script is not needed if using bearer token auth
 var btoa = require("btoa");
 var cjs = require("crypto-js");
 
+// Get API credentials from environment variables
 var api_id = pm.environment.get("api_id");
 var api_key = pm.environment.get("api_key");
 
+// Get the HTTP method from the request
 var http_verb = pm.request.method;
+
+// Extract the resource path from the request URL
 var resource_path = pm.request.url
   .toString()
   .replace(/(^{{url}})([^\?]+)(\?.*)?/, "$2");
+
+// Get the current time in epoch format
 var epoch = new Date().getTime();
 
+// If the request includes a payload, included it in the request variables
 var request_vars =
   http_verb == "GET" || http_verb == "DELETE"
     ? http_verb + epoch + resource_path
     : http_verb + epoch + pm.request.data + resource_path;
 
+// Generate the signature and build the Auth header
 var signature = btoa(cjs.HmacSHA256(request_vars, api_key).toString());
 var auth = "LMv1 " + api_id + ":" + signature + ":" + epoch;
 
+// Write the Auth header to the environment variable
 pm.environment.set("auth", auth);
