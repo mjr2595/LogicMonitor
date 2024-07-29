edb50de3581c17860f5fb819adf088f15f821af7
mjr2595 <michaeljohnraymond@gmail.com>
2024-07-28 22:23:42 -0500

    formatting

--- Scripting Other/ssh_test_all_auth.groovy
+++ Scripting Other/ssh_test_all_auth.groovy
@@ -1,28 +1,28 @@
+import com.jcraft.jsch.ChannelExec
 import com.jcraft.jsch.JSch
 import com.jcraft.jsch.Session
-import com.jcraft.jsch.ChannelExec
 import com.santaba.agent.groovyapi.expect.Expect
 import java.io.ByteArrayOutputStream
 import net.schmizz.sshj.SSHClient
-import net.schmizz.sshj.userauth.keyprovider.KeyProvider
 import net.schmizz.sshj.transport.verification.PromiscuousVerifier
-   
+import net.schmizz.sshj.userauth.keyprovider.KeyProvider
+
 // Setup connection properties
-hostname = hostProps.get("system.hostname")
-userid = hostProps.get("ssh.user")
-passwd = hostProps.get("ssh.pass")
-port = hostProps.get("ssh.port") ?: "22"
-cert = hostProps.get("ssh.cert") ?: '~/.ssh/id_rsa'
-lib = "jsch"
-   
+hostname = hostProps.get('system.hostname')
+userid = hostProps.get('ssh.user')
+passwd = hostProps.get('ssh.pass')
+port = hostProps.get('ssh.port') ?: '22'
+cert = hostProps.get('ssh.cert') ?: '~/.ssh/id_rsa'
+lib = 'jsch'
+
 useSSHJ = false // Change this to true to test sshj
-   
+
 timeout = 10000
 
-//Here is where you pass any terminal command relevant to the targeted device 
+//Here is where you pass any terminal command relevant to the targeted device
 String command = 'show version'
 String output
-  
+
 // The main conditional logic where the appropriate function is called based on the conditions
 if (useSSHJ) {
     if (passwd) {
@@ -41,29 +41,29 @@ if (useSSHJ) {
         output = sshConnectionWithCertJSch(command)
     }
 }
-   
+
 def sshConnectionWithPasswordSSHJ(String command) {
     SSHClient ssh = new SSHClient()
     ssh.addHostKeyVerifier(new PromiscuousVerifier())
-   
+
     ByteArrayOutputStream outputStream = new ByteArrayOutputStream()
-   
+
     try {
         ssh.connect(hostname, port.toInteger())
         ssh.authPassword(userid, passwd)
-        println "SSHJ session established with password authentication."
-   
+        println 'SSHJ session established with password authentication.'
+
         def session = ssh.startSession()
         try {
             def cmd = session.exec(command)
             cmd.join()
-               
+
             byte[] buffer = new byte[1024]
             int read
             while ((read = cmd.getInputStream().read(buffer)) != -1) {
                 outputStream.write(buffer, 0, read)
             }
-            println "Command execution completed with SSHJ using password."
+            println 'Command execution completed with SSHJ using password.'
         } finally {
             session.close()
         }
@@ -74,29 +74,29 @@ def sshConnectionWithPasswordSSHJ(String command) {
     println "stdout: \n$output"
     return output
 }
-   
+
 def sshConnectionWithCertSSHJ(String command) {
     SSHClient ssh = new SSHClient()
     ssh.addHostKeyVerifier(new PromiscuousVerifier())
-   
+
     ByteArrayOutputStream outputStream = new ByteArrayOutputStream()
-   
+
     try {
         ssh.connect(hostname, port.toInteger())
         ssh.authPublickey(userid, ssh.loadKeys(cert))
-        println "SSHJ session established with certificate authentication."
-   
+        println 'SSHJ session established with certificate authentication.'
+
         def session = ssh.startSession()
         try {
             def cmd = session.exec(command)
             cmd.join()
-               
+
             byte[] buffer = new byte[1024]
             int read
             while ((read = cmd.getInputStream().read(buffer)) != -1) {
                 outputStream.write(buffer, 0, read)
             }
-            println "Command execution completed with SSHJ using certificate."
+            println 'Command execution completed with SSHJ using certificate.'
         } finally {
             session.close()
         }
@@ -107,90 +107,89 @@ def sshConnectionWithCertSSHJ(String command) {
     println "stdout: \n$output" // Ensuring consistency in output printing
     return output
 }
-   
+
 def sshConnectionWithPasswordJSch(String command) {
     JSch jsch = new JSch()
     Session session = jsch.getSession(userid, hostname, port.toInteger())
     session.setPassword(passwd)
-    session.setConfig("StrictHostKeyChecking", "no")
+    session.setConfig('StrictHostKeyChecking', 'no')
     session.setTimeout(timeout)
     session.connect()
-    println "SSH session established with JSch using password authentication."
-  
-    ChannelExec channel = (ChannelExec) session.openChannel("exec")
+    println 'SSH session established with JSch using password authentication.'
+
+    ChannelExec channel = (ChannelExec) session.openChannel('exec')
     channel.setCommand(command)
-  
+
     channel.setInputStream(null)
-  
+
     ByteArrayOutputStream outputStream = new ByteArrayOutputStream()
     ByteArrayOutputStream errorStream = new ByteArrayOutputStream()
-  
+
     channel.setOutputStream(outputStream)
     channel.setErrStream(errorStream)
-  
+
     channel.connect()
-  
+
     while (!channel.isClosed()) {
         Thread.sleep(100)
     }
-      
-    println "Command execution completed with JSch using password."
+
+    println 'Command execution completed with JSch using password.'
     String output = outputStream.toString()
     String error = errorStream.toString()
-  
+
     channel.disconnect()
     session.disconnect()
-    println "SSH session disconnected with JSch using password."
-  
+    println 'SSH session disconnected with JSch using password.'
+
     if (!error.isEmpty()) {
         println "stderr: \n$error"
     }
-  
+
     println "stdout: \n$output"
     return output
 }
- 
- 
+
 def sshConnectionWithCertJSch(String command) {
     JSch jsch = new JSch()
     jsch.addIdentity(cert)  // Load the certificate
     Session session = jsch.getSession(userid, hostname, port.toInteger())
-    session.setConfig("StrictHostKeyChecking", "no")
+    session.setConfig('StrictHostKeyChecking', 'no')
     session.setTimeout(timeout)
     session.connect()
-    println "SSH session established with JSch using certificate authentication."
- 
-    ChannelExec channel = (ChannelExec) session.openChannel("exec")
+    println 'SSH session established with JSch using certificate authentication.'
+
+    ChannelExec channel = (ChannelExec) session.openChannel('exec')
     channel.setCommand(command)
- 
+
     channel.setInputStream(null)
- 
+
     ByteArrayOutputStream outputStream = new ByteArrayOutputStream()
     ByteArrayOutputStream errorStream = new ByteArrayOutputStream()
- 
+
     channel.setOutputStream(outputStream)
     channel.setErrStream(errorStream)
- 
+
     channel.connect()
- 
+
     while (!channel.isClosed()) {
         Thread.sleep(100)
     }
- 
-    println "Command execution completed with JSch using certificate."
+
+    println 'Command execution completed with JSch using certificate.'
     String output = outputStream.toString()
     String error = errorStream.toString()
- 
+
     channel.disconnect()
     session.disconnect()
-    println "SSH session disconnected with JSch using certificate."
- 
+    println 'SSH session disconnected with JSch using certificate.'
+
     if (!error.isEmpty()) {
         println "stderr: \n$error"
     }
- 
+
     println "stdout: \n$output"
     return output
 }
-   
+
 return 0
