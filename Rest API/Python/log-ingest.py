9b579f5f7911911552af4aa95a2fbd9585eb9542
mjr2595 <michaeljohnraymond@gmail.com>
2024-07-30 18:01:03 -0500

    Update log-ingest.py and .groovylintrc.json formatting

--- Rest API/Python/log-ingest.py
+++ Rest API/Python/log-ingest.py
@@ -7,42 +7,48 @@ import base64
 import time
 import hmac
 
-#Account Info
-AccessId =''
-AccessKey =''
-Company = ''
+# Account Info
+AccessId = ""
+AccessKey = ""
+Company = ""
 
-#Request Info
-httpVerb = 'POST'
-resourcePath = '/log/ingest'
-data = '{msg: "user john logged in",timestamp: 1582555792,_lm.resourceId: {system.deviceId: "281"},host: "systemY",username: "john",type_of_auth: "oauth"}'
-queryParams = ''
+# Request Info
+httpVerb = "POST"
+resourcePath = "/log/ingest"
+data = """[
+  {
+    "msg": "this is a test",
+    "_lm.resourceId": {
+      "system.deviceId": "281"
+    }
+  }
+]
+"""
+queryParams = ""
 
-#Construct URL 
-url = 'https://'+ Company +'.logicmonitor.com/santaba/rest' + resourcePath + queryParams
+# Construct URL
+url = "https://" + Company + ".logicmonitor.com/rest" + resourcePath + queryParams
 
-#Get current time in milliseconds
+# Get current time in milliseconds
 epoch = str(int(time.time() * 1000))
 
-#Concatenate Request details
-requestVars = httpVerb + epoch + data + resourcePath 
+# Concatenate Request details
+requestVars = httpVerb + epoch + data + resourcePath
 
-#Construct signature
+# Construct signature
 digest = hmac.new(
-        AccessKey.encode('utf-8'),
-        msg=requestVars.encode('utf-8'),
-        digestmod=hashlib.sha256
+    AccessKey.encode("utf-8"), msg=requestVars.encode("utf-8"), digestmod=hashlib.sha256
 ).hexdigest()
-signature = base64.b64encode(digest.encode('utf-8')).decode('utf-8')
+signature = base64.b64encode(digest.encode("utf-8")).decode("utf-8")
 
-#Construct headers
-auth = 'LMv1 ' + AccessId + ':' + str(signature) + ':' + epoch
-headers = {'Content-Type':'application/json','Authorization':auth,'X-Version':'2'}
+# Construct headers
+auth = "LMv1 " + AccessId + ":" + str(signature) + ":" + epoch
+headers = {"Content-Type": "application/json", "Authorization": auth, "X-Version": "2"}
 
-#Make request
+# Make request
 response = requests.post(url, data=data, headers=headers)
 
-#Print status and body of response
-print(httpVerb + ' ' + url)
-print('Response Status:',response.status_code)
-print('Response Body:',response.content)
+# Print status and body of response
+print(httpVerb + " " + url)
+print("Response Status:", response.status_code)
+print("Response Body:", response.content)
