f21d702f63cbe7671e51b15bd7226c541d356bc7
mjr2595 <michaeljohnraymond@gmail.com>
2025-01-28 16:34:45 -0600

    cleaned up ssh test script

--- Scripting Other/ssh_test.groovy
+++ Scripting Other/ssh_test.groovy
@@ -1,36 +1,34 @@
 import com.santaba.agent.groovyapi.expect.Expect;
+import com.santaba.agent.util.Settings
  
-hostname = hostProps.get("system.hostname");
-userid = hostProps.get("ssh.user");
-passwd = hostProps.get("ssh.pass");
-port = hostProps.get("ssh.port") ?: "22"
-lib = "jsch"
- 
-useSSHJ = false // Change this to true to test SSHJ.
- 
-if(useSSHJ) {
-    lib = "sshj"
+def hostname = hostProps.get("system.hostname")
+def user = hostProps.get("ssh.user")
+def pass = hostProps.get("ssh.pass")
+def port = hostProps.get("ssh.port", "22").toInteger()
+def timeout = hostProps.get("ssh.timeout") ?: Settings.getGroovyExpectTimeoutInSecond() ?: 10
+def lib = "sshj"
+
+useJSCH = false // Change this to true to test the older JSCH
+
+if (useJSCH) {
+    lib = "jsch"
 }
- 
-// timeout
-timeout = 10
- 
-// initiate an ssh connection to the host using the provided credentials
-ssh_connection = Expect.open(hostname, port.toInteger(), userid, passwd, timeout,"lib=${lib}");
- 
-sleep(1000);
- 
-// send a command
-ssh_connection.send('show ?\n');
- 
-sleep(1000);
- 
-// print the output output
-println "stdout " + ssh_connection.stdout();
- 
-// Logout
-ssh_connection.send('exit\n')
- 
-// close the ssh connection handle then print the config
-ssh_connection.expectClose();
-return 0;
+
+// open the ssh connection
+def cli = Expect.open(hostname, port, nimuser, nimpass, timeout, "lib=${lib}")
+
+sleep(1000)
+
+// send a command, just newline to get the prompt
+cli.send('\n\r')
+
+sleep(1000)
+
+println "stdout:\n" + cli.stdout()
+
+// logout
+cli.send('exit\n')
+
+// close the ssh connection
+cli.expectClose()
+return 0
