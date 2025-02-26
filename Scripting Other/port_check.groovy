7dfa51f7ec898d78001ee2c93f5ac38dc9cc58b0
mjr2595 <michaeljohnraymond@gmail.com>
2025-02-26 14:31:02 -0600

    formatting for port_check

--- Scripting Other/port_check.groovy
+++ Scripting Other/port_check.groovy
@@ -1,20 +1,19 @@
 import groovy.time.*
 
-hostname = hostProps.get("system.hostname");
-ports = [443, 80, 22]
+hostname = hostProps.get("system.hostname")
+ports = [443, 80, 22, 6379]
 
 def startTime = new Date()
 
 ports.each { port ->
-try {
-  Socket socket = new Socket(hostname,port)
-  println("Port ${port}: OPEN")
-} catch (e) {
-  println("Port ${port}: CLOSED")
-  }
+    try {
+        Socket socket = new Socket(hostname,port)
+        println("Port ${port}: OPEN")
+    } catch (e) {
+        println("Port ${port}: CLOSED")
+    }
 }
 def endTime = new Date()
 TimeDuration duration = TimeCategory.minus(endTime, startTime)
 
-println ""
-println "It took ${duration} to test the port.."
+println "\nIt took ${duration} to test the port.."
