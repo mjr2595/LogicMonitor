d9349ff86b7ea296b95a17f026fa70fdb65283b5
mjr2595 <michaeljohnraymond@gmail.com>
2025-07-13 19:31:21 -0500

    big ilp test ds

---
+++ LogicModules/DataSources/Test_Big_ILP/ad.groovy
@@ -0,0 +1,43 @@
+import com.logicmonitor.mod.Snippets
+import com.santaba.agent.groovy.utils.GroovyScriptHelper as GSH
+
+def modLoader = GSH.getInstance(GroovySystem.version).getScript("Snippets", Snippets.getLoader()).withBinding(getBinding())
+def emit = modLoader.load("lm.emit", "0")
+
+def ilp = [
+        "auto.a": randomString(10),
+        "auto.b": randomString(10),
+        "auto.c": randomString(48980)
+]
+
+//def ilp1 = randomString(24500)
+//def ilp2 = randomString(24500)
+//def ilp3 = randomString(16500)
+
+for (int i = 1; i <= 4; i++) {
+    def wildvalue = "instance${i}_value"
+    def wildalias = "instance${i}_alias"
+    def description = "instance${i}_description"
+    emit.instance(wildvalue, wildalias, description, ilp)
+    //println("${wildvalue}##${wildalias}##${description}####a=${ilp1}&b=${ilp2}")
+    //println("${wildvalue}##${wildalias}##${description}####a=${ilp1}&b=${ilp2}&c=${ilp3}")
+}
+
+String randomString(int length) {
+    String prefix = String.valueOf(length)
+    int prefixLen = prefix.length()
+
+    if (length < prefixLen) {
+        throw new IllegalArgumentException("Length must be at least ${prefixLen} to include its own value as prefix.")
+    }
+
+    int remaining = length - prefixLen
+    final String chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    final Random random = new Random()
+    final StringBuilder sb = new StringBuilder(length)
+    sb.append(prefix)
+    for (int i = 0; i < remaining; i++) {
+        sb.append(chars.charAt(random.nextInt(chars.length())))
+    }
+    return sb.toString()
+}
---
+++ LogicModules/DataSources/Test_Big_ILP/collect.groovy
@@ -0,0 +1,43 @@
+// datapoint1
+println "instance1_value.key1=11"
+println "instance2_value.key1=21"
+println "instance3_value.key1=31"
+println "instance4_value.key1=41"
+
+// datapoint2
+println "instance1_value.key2=12"
+println "instance2_value.key2=22"
+println "instance3_value.key2=32"
+println "instance4_value.key2=42"
+
+// JSON equivalent
+/*
+{ 
+  data: {
+    instance1_value: {
+      values: {
+        "key1": 11,
+        "key2": 12
+      }
+    },
+    instance2_value: {
+      values: {
+        "key1": 21,
+        "key2": 22
+      }
+    },
+    instance3_value: {
+      values: {
+        "key1": 31,
+        "key2": 32
+      }
+    },
+    instance4_value: {
+      values: {
+        "key1": 41,
+        "key2": 42
+      }
+    }
+  }
+}
+*/
